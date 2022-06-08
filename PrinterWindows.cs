01288e30f1e9736df49bf2a3e7d6b60ecb3077cc
Primasbt <91203079+primasbt@users.noreply.github.com>
2022-06-09 01:31:06 +0700

    Add files via upload

---
+++ PrinterWindows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Polymorphism
+{
+    public class PrinterWindows
+    {
+        public string name { get; set; }
+
+        public virtual void Show()
+        {
+
+        }
+
+        public virtual void Print()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+
+            Console.WriteLine("Select Printer: ");
+            Console.WriteLine("1. Epson ");
+            Console.WriteLine("2. Canon ");
+            Console.WriteLine("3. LaserJet\n ");
+
+            Console.Write("Printer numbers [1...3]: ");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorPrinter == 1)
+            {
+                printer = new Epson();
+            }
+            else if (nomorPrinter == 2)
+            {
+                printer = new Canon();
+            }
+            else
+            {
+                printer = new Laser();
+            }
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
